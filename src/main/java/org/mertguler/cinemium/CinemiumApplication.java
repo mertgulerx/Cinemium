b5b97d8f53762c140e423a611e1ff780f679e4f7
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-23 20:07:13 +0300

    Initial spring initialize

---
+++ src/main/java/org/mertguler/cinemium/CinemiumApplication.java
@@ -0,0 +1,13 @@
+package org.mertguler.cinemium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mertguler/cinemium/CinemiumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mertguler.cinemium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
