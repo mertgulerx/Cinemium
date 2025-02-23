c8df7d13abf0680f2a13f3df16dce4df59d39438
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 00:04:33 +0300

    add CinemaController

---
+++ src/main/java/org/mertguler/cinemium/controller/CinemaController.java
@@ -0,0 +1,38 @@
+package org.mertguler.cinemium.controller;
+
+import jakarta.validation.Valid;
+import org.mertguler.cinemium.payload.dto.CinemaDTO;
+import org.mertguler.cinemium.payload.response.CinemaResponse;
+import org.mertguler.cinemium.service.cinema.CinemaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CinemaController {
+
+    @Autowired
+    private CinemaService cinemaService;
+
+    @GetMapping("/public/cinemas")
+    public ResponseEntity<CinemaResponse> getAllCategories(){
+        CinemaResponse cinemaResponse = cinemaService.getAllCinemas();
+        return new ResponseEntity<>(cinemaResponse, HttpStatus.OK);
+    }
+
+    @PostMapping("/public/cinemas")
+    public ResponseEntity<CinemaDTO> createCinema(@Valid @RequestBody CinemaDTO cinemaDTO){
+        CinemaDTO savedCinemaDTO = cinemaService.createCinema(cinemaDTO);
+        return new ResponseEntity<>(savedCinemaDTO, HttpStatus.OK);
+    }
+
+    @PutMapping("/public/cinemas/{cinemaId}")
+    public ResponseEntity<CinemaDTO> updateCategory(@Valid @RequestBody CinemaDTO cinemaDTO, @PathVariable Long cinemaId){
+        CinemaDTO savedCinemaDTO = cinemaService.updateCinema(cinemaDTO, cinemaId);
+        return new ResponseEntity<>(savedCinemaDTO, HttpStatus.OK);
+    }
+
+
+}
