9e5c52d260dba5a5f13ef1b80bcd824d17371166
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 00:06:25 +0300

    add Enums

---
+++ src/main/java/org/mertguler/cinemium/model/seat/SeatStatus.java
@@ -0,0 +1,8 @@
+package org.mertguler.cinemium.model.seat;
+
+public enum SeatStatus {
+    AVAILABLE,
+    BOOKED,
+    PENDING,
+    FAULTY
+}
---
+++ src/main/java/org/mertguler/cinemium/model/seat/SeatType.java
@@ -0,0 +1,7 @@
+package org.mertguler.cinemium.model.seat;
+
+public enum SeatType {
+    REGULAR,
+    COUPLE,
+    ACCESSIBLE
+}
