60be46ee9db47bf91e7caf643b9b7c17f2ac9495
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 14:19:00 +0300

    add SeatDTO

---
+++ src/main/java/org/mertguler/cinemium/payload/dto/SeatDTO.java
@@ -0,0 +1,38 @@
+package org.mertguler.cinemium.payload.dto;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.mertguler.cinemium.model.building.seat.SeatStatus;
+import org.mertguler.cinemium.model.building.seat.SeatType;
+import org.mertguler.cinemium.validator.EnumValidator;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatDTO {
+    private Long seatId;
+
+    @NotNull
+    @Min(0)
+    private Integer rowIndex;
+
+    @NotNull
+    @Min(0)
+    private Integer columnIndex;
+
+    //     "type": "must be any of enum class org.mertguler.cinemium.model.building.seat.SeatType"
+    @EnumValidator(enumClass = SeatType.class)
+    private String type;
+
+    @EnumValidator(enumClass = SeatStatus.class)
+    private String status;
+
+    private Long stageId;
+}
