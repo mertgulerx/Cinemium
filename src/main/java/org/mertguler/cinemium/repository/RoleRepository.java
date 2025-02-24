8d094d74c98e958f5632c1885abee4dbe297f933
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 16:53:28 +0300

    add RoleRepository.java

---
+++ src/main/java/org/mertguler/cinemium/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package org.mertguler.cinemium.repository;
+
+import org.mertguler.cinemium.model.user.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.nio.file.LinkOption;
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByRoleName(String string);
+}
