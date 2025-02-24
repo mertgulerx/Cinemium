df36b3276b37df60929c828d97e3d3965778ac00
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 14:19:09 +0300

    add SeatRepository

---
+++ src/main/java/org/mertguler/cinemium/repository/SeatRepository.java
@@ -0,0 +1,19 @@
+package org.mertguler.cinemium.repository;
+
+import org.mertguler.cinemium.model.building.Stage;
+import org.mertguler.cinemium.model.building.seat.Seat;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+    @Query("SELECT seat FROM Seat seat WHERE seat.stage.stageId = ?1")
+    Optional<List<Seat>> findAllByStageId(Long stageId);
+
+    @Query("SELECT seat FROM Seat seat WHERE seat.stage.stageId = ?1 AND seat.rowIndex = ?2 AND seat.columnIndex = ?3")
+    Seat findSeatByStageIdAndPosition(Long stageId, Integer rowIndex, Integer columnIndex);
+}
