242dfe319df23bd74d759ac475c8f16141891a66
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 14:19:27 +0300

    add SeatService

---
+++ src/main/java/org/mertguler/cinemium/service/seat/SeatService.java
@@ -0,0 +1,15 @@
+package org.mertguler.cinemium.service.seat;
+
+import jakarta.validation.Valid;
+import org.mertguler.cinemium.payload.dto.SeatDTO;
+import org.mertguler.cinemium.payload.response.SeatResponse;
+
+public interface SeatService {
+    SeatResponse getSeats(Long stageId);
+
+    SeatDTO createSeat(@Valid SeatDTO seatDTO, Long stageId);
+
+    SeatDTO updateSeat(@Valid SeatDTO seatDTO, Long seatId);
+
+    SeatDTO deleteSeat(Long seatId);
+}
