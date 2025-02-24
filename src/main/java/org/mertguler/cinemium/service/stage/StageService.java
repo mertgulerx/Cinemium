310a67f39477a1ea60e64887657d7c204da0e91a
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 14:18:23 +0300

    add StageService

---
+++ src/main/java/org/mertguler/cinemium/service/stage/StageService.java
@@ -0,0 +1,15 @@
+package org.mertguler.cinemium.service.stage;
+
+import jakarta.validation.Valid;
+import org.mertguler.cinemium.payload.dto.StageDTO;
+import org.mertguler.cinemium.payload.response.StageResponse;
+
+public interface StageService {
+    StageResponse getStages(Long cinemaId);
+
+    StageDTO createStage(@Valid StageDTO stageDTO, Long cinemaId);
+
+    StageDTO updateStage(@Valid StageDTO stageDTO, Long stageId);
+
+    StageDTO deleteStage(Long stageId);
+}
