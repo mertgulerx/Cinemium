340b179bab7bb28fad170c715d742e6db01652a1
mertgulerx <108007620+mertgulerx@users.noreply.github.com>
2025-02-24 16:52:49 +0300

    add MessageResponse.java

---
+++ src/main/java/org/mertguler/cinemium/security/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package org.mertguler.cinemium.security.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
